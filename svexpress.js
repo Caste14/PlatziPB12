6d606f47882fcbe6c0ff35cf78410eaa2e2bc958
caste_laro <santinba6@gmail.com>
2022-06-30 21:52:38 -0300

    30/06

--- svexpress.js
+++ svexpress.js
@@ -1,1 +1,18 @@
+const { application } = require("express");
 var express = require ("express");
+var app = express();
+
+application.get("/", inicio);
+application.get("/", cursos)
+
+function inicio(peticion, resultado)
+{
+    resultado.send("Home truco")
+}
+
+function cursos(peticion, resultado)
+{
+
+}
+
+application.listen(8080);
