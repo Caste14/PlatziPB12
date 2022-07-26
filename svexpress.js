c8ee626e58462dc1ffc9981edeb022a86d80df68
caste_laro <santinba6@gmail.com>
2022-07-26 15:07:06 -0300

    correciones

--- svexpress.js
+++ svexpress.js
@@ -1,18 +1,28 @@
-const { application } = require("express");
-var express = require ("express");
-var app = express();
+var cafecito = require("express");
+var aplicacion = cafecito();
 
-application.get("/", inicio);
-application.get("/", cursos)
+aplicacion.get("/", inicio);
+aplicacion.get("/cursos", cursos);
 
 function inicio(peticion, resultado)
 {
-    resultado.send("Home truco")
+    resultado.send("Este es el <strong>home</strong>");
 }
 
 function cursos(peticion, resultado)
 {
-
+    resultado.send("Estos son los <strong>cursos</strong>");
 }
 
-application.listen(49718);
+aplicacion.listen(8989);
+
+//En la primera línea tienes una const al inicio que no me queda claro para qué la utilizas.
+
+
+//En la línea 6 falta el punto y coma al terminar la línea de codigo " ; "
+
+//En la línea 10 falta el punto y coma " ; "
+
+//En la línea 15 no colocaste nada
+
+//en la línea 18 tu puerto es “49718” cuando debería ser aplicacion.listen(8989);
